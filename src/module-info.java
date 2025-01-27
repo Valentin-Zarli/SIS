c955a0b13098a52f04978b79ac96d0b50514f3ec
valen <valen@147.171.172.91>
2025-01-27 14:33:17 +0100

    Version 1

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module SIS1 {
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires java.sql;
+    opens ui to javafx.graphics;
+    exports ui;  // Si ton code UI est dans ce package
+    
+
+      // Exporter le package ui si tu veux qu'il soit accessible
+    exports fc;  // Exporter le package fc si tu veux qu'il soit accessible
+    opens fc to javafx.fxml;
+}
